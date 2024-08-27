46b8caf90e48b439823bfbadd3ba0f4f527fb97a
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-27 23:32:46 +0530

    new keyword, it sends request to the class to create object

---
+++ Core-Java/src/ObjectCreation.java
@@ -0,0 +1,9 @@
+
+public class ObjectCreation {
+	public static void main(String[] args) {
+		ObjectCreation a1 = new ObjectCreation();
+		System.out.println(a1);
+		ObjectCreation a2 = new ObjectCreation();
+		System.out.println(a2);
+	}
+}
