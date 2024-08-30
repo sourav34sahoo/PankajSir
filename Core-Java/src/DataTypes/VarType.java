69793fc68fc1cb33934ab931369f42e1fc91057a
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-31 00:57:33 +0530

    Program for var type

---
+++ Core-Java/src/DataTypes/VarType.java
@@ -0,0 +1,21 @@
+package DataTypes;
+
+public class VarType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var x1 = 10;
+		var x2 = 10.3f;
+		var x3 = 'a';
+		var x4 = "sourav";
+	    var x5 = new VarType();
+	    
+	    System.out.println(x1);  // 10
+	    System.out.println(x2);  // 10.3
+	    System.out.println(x3);  // a
+	    System.out.println(x4);  // sourav
+	    System.out.println(x5);  // DataTypes.VarType@7960847b
+	}
+}
+
+// var type can be used in local variable , it can't be static and non-static variable.
