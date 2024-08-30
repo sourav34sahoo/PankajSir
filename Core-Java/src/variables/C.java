78a01e946231e277e7d2d71500a3add9e5002824
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-31 00:04:52 +0530

    Program2 for Stack Heap concept

---
+++ Core-Java/src/variables/C.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class C {
+	public static void main(String[] args) {
+		C c1 = new C();
+		c1.Test();
+	}
+	public void Test() {
+		System.out.println(100);     // 100
+	}
+
+}
