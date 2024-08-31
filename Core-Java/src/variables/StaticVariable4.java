d37395da13e794c85a2844575ee697c99ff54396
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-31 22:49:03 +0530

    Program4 for Static variables

---
+++ Core-Java/src/variables/StaticVariable4.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class StaticVariable4 {
+	static int x ;
+	public static void main(String[] args) {
+		System.out.println(x);       // 0
+	}
+
+}
+/* static variables if not initialized then depending on the data type 
+automatically default value will get store in it. */ 
