46bdd369fa387c5b96e0259524c681e064373f66
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-31 22:28:48 +0530

    Program4 for Local variable

---
+++ Core-Java/src/variables/LocalVariable4.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class LocalVariable4 {
+	public static void main(String[] args) {
+		int x;
+		System.out.println(x);  // Error
+	}
+}
+
+//Without initializing local variable we can't use that variable.
