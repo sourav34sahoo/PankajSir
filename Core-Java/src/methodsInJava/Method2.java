9a67521ade3f4b244c02bf9af6f682fc6da86d50
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-05 01:46:59 +0530

    Program2 for Methods in java

---
+++ Core-Java/src/methodsInJava/Method2.java
@@ -0,0 +1,10 @@
+package methodsInJava;
+
+public class Method2 {
+	public static void main(String[] args) {
+	}
+	public int Test() {  //error
+		
+	}
+}
+// It is mandatory to write return value statement inside not a void method.
