8a7a0d6766cc0fc19d83ee8ba88ce8e113637f7c
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-07 21:10:52 +0530

    Program6 for methods in java

---
+++ Core-Java/src/methodsInJava/Method6.java
@@ -0,0 +1,15 @@
+package methodsInJava;
+
+public class Method6 {
+	public static void main(String[] args) {
+		Method6 m1 = new Method6();
+		m1.Test();
+		
+	}
+	public void Test() {
+		System.out.println(100); //100
+		return;
+	}
+}
+//only return can be used inside void method and it is optional to use.
+//return keyword will return control to method calling statement. 
