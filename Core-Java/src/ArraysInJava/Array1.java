abd880f23e2daf40f6d9620c663f5acd54dcdd43
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-11 02:33:59 +0530

    Program for Array concept

---
+++ Core-Java/src/ArraysInJava/Array1.java
@@ -0,0 +1,15 @@
+package ArraysInJava;
+
+public class Array1 {
+	public static void main(String[] args) {
+		int[] age = new int[3];
+		age[0]=36;
+		age[1]=27;
+		age[2]=28;
+		
+		System.out.println(age[0]);  //36
+		System.out.println(age[1]);  //27
+		System.out.println(age[2]);  //28
+	}
+}
+//It is a DtatStructure that holds collection of values.
