cf04493b467e3d7d28781e00683c4376af8672c6
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-11 02:44:48 +0530

    program1 for Array concept

---
+++ Core-Java/src/ArraysInJava/Array2.java
@@ -0,0 +1,14 @@
+package ArraysInJava;
+
+public class Array2 {
+	public static void main(String[] args) {
+		String[] name = new String[3];
+		name[0]="Sourav";
+		name[1]="kumar";
+		name[2]="sahoo";
+		
+		System.out.println(name[0]);  //sourav
+		System.out.println(name[1]);  //kumar
+		System.out.println(name[2]);  //sahoo
+	}
+}
