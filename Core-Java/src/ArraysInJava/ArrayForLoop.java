8916e1d3f36804e247b3dc5c99557b817ac0282c
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-11 11:36:50 +0530

    Program for Array forloop

---
+++ Core-Java/src/ArraysInJava/ArrayForLoop.java
@@ -0,0 +1,9 @@
+package ArraysInJava;
+
+public class ArrayForLoop {
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			System.out.println(i);  //0 1 2 3 4 5 6 7 8 9 
+		}
+	}
+}
