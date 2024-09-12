701fbcbe3da6610918817709091f28447b974c1a
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-12 11:41:07 +0530

    Program for Unary operators

---
+++ Core-Java/src/UnaryOperators/PostIncrementD.java
@@ -0,0 +1,11 @@
+package UnaryOperators;
+
+public class PostIncrementD {
+	public static void main(String[] args) {
+		int i = 10;                            
+		int j = i++ + i++ + i + i++ + i + i++;
+		System.out.println(i);    //14
+		System.out.println(j);    //71
+	}
+
+}
