0828278e2439f1ef8b0e538348ff3aed8dd23d7a
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-12 12:02:29 +0530

    Program1 for Pre increment

---
+++ Core-Java/src/UnaryOperators/PreIncrementA.java
@@ -0,0 +1,11 @@
+package UnaryOperators;
+
+public class PreIncrementA {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = ++i + ++i;
+		System.out.println(i);   //12
+		System.out.println(j);   //23
+	}
+
+}
