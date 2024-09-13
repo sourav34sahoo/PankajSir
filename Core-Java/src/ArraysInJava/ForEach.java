f3eb05c01164976d47340a34db47ba55610cc4f3
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-14 00:43:37 +0530

    Program  for each condition

---
+++ Core-Java/src/ArraysInJava/ForEach.java
@@ -0,0 +1,11 @@
+package ArraysInJava;
+
+public class ForEach {
+	public static void main(String[] args) {
+		int[] x = {1,2,3,4,5};
+		for(int i:x) {
+			System.out.println(i);  // 1 2 3 4 5
+		}
+	}
+
+}
