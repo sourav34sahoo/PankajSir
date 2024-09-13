ad9ad961a9e171fc62a858622041654e7ad23f16
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-13 23:55:26 +0530

    Program for Do while loop

---
+++ Core-Java/src/ArraysInJava/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package ArraysInJava;
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println(x);  //10
+		x--;
+		while(x>0) {
+		}
+	}
+
+}
+/* For the first time it doesn't matter wheather the condition is true or false first time
+it will ru the second time for it to run depend on the condition */
