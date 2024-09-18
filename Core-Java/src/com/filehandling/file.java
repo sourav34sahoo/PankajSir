6298b7cf47c37b9de61cbc7e7ce9c963e95048af
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-18 20:05:58 +0530

    program for File handling

---
+++ Core-Java/src/com/filehandling/file.java
@@ -0,0 +1,10 @@
+package com.filehandling;
+
+import java.io.File;
+public class file {
+	public static void main(String[] args) {
+		File f = new File("D:\\PankajSir\\file.txt");   
+		System.out.println(f);        // o/p D:\PankajSir\file.txt
+	}
+
+}
