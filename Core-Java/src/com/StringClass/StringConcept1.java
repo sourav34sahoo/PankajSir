c5a3ce69a5a5219a734c1c0983be973be1a1268b
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-27 23:44:45 +0530

    Program for store String values

---
+++ Core-Java/src/com/StringClass/StringConcept1.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class StringConcept1 {
+	public static void main(String[] args) {
+		String s1 = "Sourav";
+		String s2 = "Sourav";
+		
+		System.out.println(s1==s2);
+	}
+}
