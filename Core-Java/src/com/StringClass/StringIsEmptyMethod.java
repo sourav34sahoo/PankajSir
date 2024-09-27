b4f2f54237d952fb00646acb441629b8770b17e0
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-28 00:44:06 +0530

    program for String isEmpty()

---
+++ Core-Java/src/com/StringClass/StringIsEmptyMethod.java
@@ -0,0 +1,14 @@
+package com.StringClass;
+
+public class StringIsEmptyMethod {
+	public static void main(String[] args) {
+		String s1 = "";
+		
+		//option 1
+		System.out.println(s1.isEmpty());   // return boolean value  true
+		
+		//option 2
+		System.out.println(s1.length());    // 0
+	}
+
+}
