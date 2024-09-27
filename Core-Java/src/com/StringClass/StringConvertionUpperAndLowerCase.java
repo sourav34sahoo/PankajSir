da3cfba9334a9c40c307f7baefe2f87f1f2e4889
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-28 01:34:03 +0530

    program for String convertion of toUpperCase() & toLowerCase()

---
+++ Core-Java/src/com/StringClass/StringConvertionUpperAndLowerCase.java
@@ -0,0 +1,11 @@
+package com.StringClass;
+
+public class StringConvertionUpperAndLowerCase {
+	public static void main(String[] args) {
+		String s1 = "aabC";
+		String s2 = "ABcD";
+		
+		System.out.println(s1.toUpperCase());    // AABC
+		System.out.println(s2.toLowerCase());	 // abcd
+	}
+}
