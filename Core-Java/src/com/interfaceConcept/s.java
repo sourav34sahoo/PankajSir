7d1bb70d1999b02e577d9ca1a42bd12a68a8cf19
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-05 18:17:54 +0530

    program for add inImplemented methods

---
+++ Core-Java/src/com/interfaceConcept/s.java
@@ -0,0 +1,17 @@
+package com.interfaceConcept;
+
+public class s implements r {
+
+	@Override
+	public void test1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
