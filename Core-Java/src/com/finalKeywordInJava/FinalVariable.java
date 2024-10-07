74e1f38399a3352b7a9668d08c6b07a3d6fbeaea
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-08 01:04:39 +0530

    program for final variable concept

---
+++ Core-Java/src/com/finalKeywordInJava/FinalVariable.java
@@ -0,0 +1,10 @@
+package com.finalKeywordInJava;
+
+public class FinalVariable {
+	public static void main(String[] args) {
+		final int x = 10;
+		//x = 10;   Error
+	}
+
+}
+// If we make a variable final the re-initialization is not allowed
