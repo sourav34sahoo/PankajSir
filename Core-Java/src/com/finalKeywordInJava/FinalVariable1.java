21f03f7467d0c0f5dddc1e0681d603cd2a2d6938
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-08 01:11:05 +0530

    program1 for final variable concept

---
+++ Core-Java/src/com/finalKeywordInJava/FinalVariable1.java
@@ -0,0 +1,12 @@
+package com.finalKeywordInJava;
+
+public class FinalVariable1 {
+	final int x ; // blank final field
+	final static int x ; // blank final field 
+	public static void main(String[] args) {
+		
+	}
+
+}
+/* if we make static or non static variable final then it is mandatory to initialize
+ * it or else we will get blank final field error  */
