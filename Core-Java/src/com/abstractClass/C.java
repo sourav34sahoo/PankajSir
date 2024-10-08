58166b328f102133cdd54f8228ac5433966de002
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-09 00:02:55 +0530

    program for class extends abstract class

---
+++ Core-Java/src/com/abstractClass/C.java
@@ -0,0 +1,18 @@
+package com.abstractClass;
+
+public class C extends B{
+
+	@Override
+	public void test1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test2() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
