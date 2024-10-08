a5471e19a9a338eb725dbf9d50f113e9cd08574d
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-08 23:52:10 +0530

    program3 for abstract class

---
+++ Core-Java/src/com/abstractClass/A3.java
@@ -0,0 +1,10 @@
+package com.abstractClass;
+
+public class A3 extends AbstractClass3 {
+	public static void main(String[] args) {
+		A3 a = new A3();
+		System.out.println(a.x);
+		System.out.println(AbstractClass3.y);
+	}
+
+}
