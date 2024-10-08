e0d049302dc24b37fdd2fcdfdd071e9a9a9c42da
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-08 23:00:46 +0530

    program for abstract class

---
+++ Core-Java/src/com/abstractClass/AbstractClass1.java
@@ -0,0 +1,11 @@
+package com.abstractClass;
+
+public abstract class AbstractClass1 {
+	public abstract void test1();
+	public void test2() {
+		System.out.println(2);
+	}
+}
+/* Here we can develop both complete and incomplete methods  in it .
+ * To develop incomplete methods in an abstract class we should use abstract keyword 
+ */
