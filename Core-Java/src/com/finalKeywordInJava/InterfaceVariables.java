d5f95570cc4ada9274a4699575c76298a396548d
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-08 22:39:47 +0530

    program for interface variables

---
+++ Core-Java/src/com/finalKeywordInJava/InterfaceVariables.java
@@ -0,0 +1,8 @@
+package com.finalKeywordInJava;
+
+public interface InterfaceVariables {
+	int AMOUNT = 100;
+	final static int CURRENT_YEAR = 2024;
+
+}
+// all variables inside interface is by defULT final & static 
