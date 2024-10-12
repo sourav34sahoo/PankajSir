4a3e78c05f8acd2ffad9496d387c5321f1cc50f7
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 18:38:06 +0530

    program for replace a particular word ina given string

---
+++ Core-Java/src/com/StringClass/ReplaceParticularWordInAGivenString.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class ReplaceParticularWordInAGivenString {
+	public static void main(String[] args) {
+		String s = "My name sourav";
+		String str = s.replace("sourav", "sahoo");
+		System.out.println(str);
+	}
+
+}
