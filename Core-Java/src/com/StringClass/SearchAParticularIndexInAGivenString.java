bcc6884b5fdfa6b54c1ac59a260d8f4836943ae3
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 12:23:04 +0530

    program for search a particular index in a given string

---
+++ Core-Java/src/com/StringClass/SearchAParticularIndexInAGivenString.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class SearchAParticularIndexInAGivenString {
+	public static void main(String[] args) {
+		String s = "Hello World";
+		System.out.println(s.indexOf("World"));   // 6
+		System.out.println(s.indexOf("world"));    // -1
+	}
+
+}
