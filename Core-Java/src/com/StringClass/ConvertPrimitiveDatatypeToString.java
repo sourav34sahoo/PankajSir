1b8e4c6683850ae82c1dededeba57fd63798a6c8
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 11:55:51 +0530

    program for convert a primitive datatype to string

---
+++ Core-Java/src/com/StringClass/ConvertPrimitiveDatatypeToString.java
@@ -0,0 +1,14 @@
+package com.StringClass;
+
+public class ConvertPrimitiveDatatypeToString {
+	public static void main(String[] args) {
+		int x = 10;
+		String y = " "+x;
+		System.out.println(y);
+		
+		float f = 10.34f;
+		String s = " "+f;
+		System.out.println(s);
+	}
+
+}
