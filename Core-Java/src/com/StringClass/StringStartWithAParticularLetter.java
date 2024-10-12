26ac075ad0e9b76c34e7032d4eabb6bfecce3892
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 11:47:42 +0530

    program for wheather a given string starts&ends with a particular letter or not

---
+++ Core-Java/src/com/StringClass/StringStartWithAParticularLetter.java
@@ -0,0 +1,9 @@
+package com.StringClass;
+
+public class StringStartWithAParticularLetter {
+	public static void main(String[] args) {
+		String s = "i am sourav";
+		System.out.println(s.startsWith("a"));    // false
+		System.out.println(s.endsWith("v"));     // true
+	}
+}
