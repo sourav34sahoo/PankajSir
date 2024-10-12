4cae9fff86109aa217472487d4e67a3e558012b7
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 19:00:07 +0530

    program for Concatination in string 1st way

---
+++ Core-Java/src/com/StringClass/StringConcatination.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class StringConcatination {
+	public static void main(String[] args) {
+		String s1 = "sourav";
+		String s2 = "sahoo";
+		System.out.println(s1+" "+s2);
+	}
+
+}
