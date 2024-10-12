c70fcf5c0e3440551495cfcf0c3fe8127594b3b0
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-12 18:43:13 +0530

    program for replace a particular character in a given string

---
+++ Core-Java/src/com/StringClass/ReplaceParticularCharacterInAGivenString.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class ReplaceParticularCharacterInAGivenString {
+	public static void main(String[] args) {
+		String s = "Hello World";
+		String str = s.replace('l', '*');
+		System.out.println(str);
+	}
+
+}
