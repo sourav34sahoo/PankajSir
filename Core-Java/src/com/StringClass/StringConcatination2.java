bab497571c2c944f1b4a48e5ad3addf7f8f2dec9
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-13 11:21:31 +0530

    program for String concatination 2nd way

---
+++ Core-Java/src/com/StringClass/StringConcatination2.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class StringConcatination2 {
+	public static void main(String[] args) {
+		String s1 = "sourav";
+		String s2 = "sahoo";
+		System.out.println(s1.concat(" ").concat(s2));
+	}
+
+}
