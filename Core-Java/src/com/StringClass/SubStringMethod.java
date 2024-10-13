854d94c83b1b866a23de3aeda3ae995b851b002b
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-13 11:24:57 +0530

    program for SubString method

---
+++ Core-Java/src/com/StringClass/SubStringMethod.java
@@ -0,0 +1,10 @@
+package com.StringClass;
+
+public class SubStringMethod {
+	public static void main(String[] args) {
+		String s = "Hello World";
+		String substr = s.substring(0,8);
+		System.out.println(substr);
+	}
+
+}
