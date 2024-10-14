9f93217c4a0fc506623c181117db68bf3e215b64
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-14 19:36:27 +0530

    program for String multiline

---
+++ Core-Java/src/com/StringClass/StringMultiline.java
@@ -0,0 +1,13 @@
+package com.StringClass;
+
+public class StringMultiline {
+	public static void main(String[] args) {
+		String s = """
+				hiii
+				i am sourav
+				Don't think muchmore about me.....
+				""";
+		System.out.println(s);
+	}
+
+}
