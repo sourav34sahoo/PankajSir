3181da330f3ae7ce4cae758a122478874ad769ae
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-16 11:47:14 +0530

    program for swapping elements

--- Core-Java/src/SwappingElements.java
+++ Core-Java/src/SwappingElements.java
@@ -1,14 +1,13 @@
 
 public class SwappingElements {
 	public static void main(String[] args) {
-		int x = 10;
-		int y = 20;
+		int x = 10;         // 20
+		int y = 20;         // 10
 		int Z = x;
 		x = y;
 		y = Z;
 		System.out.println(x);
 		System.out.println(y);
-		
 	}
 
 }
