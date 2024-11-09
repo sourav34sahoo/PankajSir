f3e0e5444d52e3968488fea4d2ecde0dbd3dc07b
sourav sahoo <sahoo34sourav@gmail.com>
2024-11-10 00:33:17 +0530

    program for wild card entry in Ganerics concept

---
+++ app_2/src/com/Ganerics/WildCardEntry.java
@@ -0,0 +1,10 @@
+package com.Ganerics;
+import java.util.*;
+public class WildCardEntry {
+    public  static void main(String...args){
+    }
+    public List<?> getList(){
+        List<Integer> arr = new ArrayList<>();
+        return arr;
+    }
+}
