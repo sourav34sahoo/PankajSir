8a8d855e58016d0fd188a360e9d2cdb4e14012bd
sourav sahoo <sahoo34sourav@gmail.com>
2024-11-10 00:27:57 +0530

    program1 for Ganerics Concept

---
+++ app_2/src/com/Ganerics/GanericsDemo.java
@@ -0,0 +1,13 @@
+package com.Ganerics;
+
+import java.util.*;
+
+public class GanericsDemo {
+    public static void main(String...args){
+        List<String> l = new ArrayList<>();
+        l.add("Sourav");
+        // l.add (2002);       Error
+        System.out.println(l);  // [Sourav]
+
+    }
+}
