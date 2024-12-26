5190efae16eac31bf9508047cdd1f9c1f412e62f
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-26 17:55:38 +0530

    program for print time in ms

---
+++ Core-Java/src/com/DateAndTime/DateAndTimeDemo2.java
@@ -0,0 +1,14 @@
+package com.DateAndTime;
+
+import java.util.*;
+public class DateAndTimeDemo2 {
+
+	public static void main(String[] args) {
+		Date d = new Date();
+		long time = d.getTime();
+		System.out.println(time);
+	}
+}
+/* 
+ * 1735215717254
+ */ 
