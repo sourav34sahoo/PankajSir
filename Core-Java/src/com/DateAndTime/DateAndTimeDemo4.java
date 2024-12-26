29eaab01b3157d0b184c6e85de63000ef3fa3cc6
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-26 18:18:22 +0530

    program for Date And Time

---
+++ Core-Java/src/com/DateAndTime/DateAndTimeDemo4.java
@@ -0,0 +1,12 @@
+package com.DateAndTime;
+
+import java.util.*;
+public class DateAndTimeDemo4 {
+
+	public static void main(String[] args) {
+		Date d = new Date(200, 1, 3);
+		System.out.println(d);
+	}
+}
+
+// O/p  Wed Feb 03 00:00:00 IST 2100
