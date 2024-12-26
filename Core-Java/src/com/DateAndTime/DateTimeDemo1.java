abdd307cf2692abef8fdf1e84f7d88f7930b7ec9
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-26 17:45:14 +0530

    progaram for print current Date & Time

---
+++ Core-Java/src/com/DateAndTime/DateTimeDemo1.java
@@ -0,0 +1,11 @@
+package com.DateAndTime;
+
+import java.util.*;
+public class DateTimeDemo1 {
+	
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println(d);
+	}
+
+}
