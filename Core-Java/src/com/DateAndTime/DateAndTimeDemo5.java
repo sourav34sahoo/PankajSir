18bab725d1db676f7e9a891bb8c3b3c5e4a98013
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-26 18:33:33 +0530

    program for sql Date And Time

---
+++ Core-Java/src/com/DateAndTime/DateAndTimeDemo5.java
@@ -0,0 +1,13 @@
+package com.DateAndTime;
+
+import java.sql.Date;
+
+public class DateAndTimeDemo5 {
+
+	public static void main(String[] args) {
+		Date d = new Date(200, 1, 3);
+		System.out.println(d);
+	}
+}
+
+// O/P 2100-02-03
